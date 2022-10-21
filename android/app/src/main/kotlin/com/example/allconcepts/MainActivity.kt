8a82d4d76679e4d04e74a6a699b08294e56b471e
stibinottathai <augustinestibin@gmail.com>
2022-10-21 14:02:21 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/allconcepts/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.allconcepts
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:allconcepts/provider/todo_provider.dart';
+import 'package:allconcepts/screens/show_todo_screen.dart';
+import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => TodoProvider(),
+        ),
+      ],
+      builder: (context, child) => const MaterialApp(
+        debugShowCheckedModeBanner: false,
+
+        home: ShowTodoScreen(),
+      ),
+    );
+  }
+}
